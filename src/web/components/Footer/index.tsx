039521c2058605fd0de0e21679332155cd575027
l-sui <943176792@qq.com>
2020-09-12 20:26:01 +0800

    add:#更改文件名

---
+++ src/web/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+// import "./banner.css";
+import * as React from "react";
+const Footer:React.FC= () => {
+  return (
+    <>
+      <div className="components-footer">
+        <span>daily</span>
+      </div>
+    </>
+  );
+};
+export default Footer;
