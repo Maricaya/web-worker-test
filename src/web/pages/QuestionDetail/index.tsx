492e83f15583922647f3617a6d249ef7131f962e
Danielhard <335805097@qq.com>
2020-09-26 17:29:42 +0800

    路由

--- src/web/pages/QuestionDetail/index.tsx
+++ src/web/pages/QuestionDetail/index.tsx
@@ -1,17 +1,24 @@
 import React, { FC, useEffect, useState } from 'react';
 import { useRootData } from '@tools/useRootData';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import './QuestionDetail.css';
 
-const QuestionDetail: FC = (): JSX.Element => {
-  const { qid, uid, title } = useRootData((store) => ({
-    qid: store.QuestionSimple.qid,
-    uid: store.QuestionSimple.uid,
-    title: store.QuestionSimple.title,
-  }));
+type HistoryType = {
+  location: object;
+};
+const QuestionDetail: FC<HistoryType> = ({ location }): JSX.Element => {
+  // console.log(location);
+  const { uid, qid, title } = useParams();
+  console.log(uid, qid, title);
+  // const { qid, uid, title } = useRootData((store) => ({
+  //   qid: store.QuestionSimple.qid,
+  //   uid: store.QuestionSimple.uid,
+  //   title: store.QuestionSimple.title,
+  // }));
   const [baseUrl, setUrl] = useState<string>('');
   const [answerFlag, setAnswerFlag] = useState<boolean>(true);
-  console.log(qid, uid);
+
   useEffect(() => {
     queryDetails();
   }, []);
